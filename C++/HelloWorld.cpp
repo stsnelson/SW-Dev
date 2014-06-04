3664b0adbab7af85cc889a3907a74c79ee4ee3df
stsnelson <steve@Stephens-MacBook-Pro.local>
2014-06-04 22:22:45 +0100

    Hello World

---
+++ C++/HelloWorld.cpp
@@ -0,0 +1,19 @@
+/*
+HelloWorld.cpp
+
+Author - Steve Snelson
+Date -	03/06/14
+
+Write "Hello World" to console
+*/
+
+// Include the standard input or Output commands
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ //cout output the string to the screen
+ cout << "Hello World";
+ return 0;
+}
